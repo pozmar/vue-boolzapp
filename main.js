e8aefef8441cac8b6cfa2eb9b1405849de391a0d
Martina <martina.pozzerle@libero.it>
2020-11-20 14:36:36 +0100

    #struttura generale

---
+++ main.js
@@ -0,0 +1,14 @@
+const myApp = new Vue({
+  el: "#root",
+  data: {
+    utentePrincipale: {
+      utente: 'img/avatar_io.jpg',
+      nome: 'Martina'
+    },
+    contatti: ['Marco', 'Fabio', 'Fabrizio', 'Alessandra'],
+  contatto: {
+    nome: 'Marco',
+    avatar: 'img/avatar_1.jpg',
+  }
+}
+})
