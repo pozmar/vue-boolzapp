b29b1210b8621e369ebb074ea872edd25ce3109b
Martina <martina.pozzerle@libero.it>
2020-11-23 11:42:22 +0100

    #sistemato js

--- main.js
+++ main.js
@@ -9,29 +9,41 @@ const myApp = new Vue({
       {
         nome: 'Marco',
         avatar: 'img/avatar_1.jpg',
-
+        ultimoAccesso:''
       },
       {
         nome: 'Fabio',
         avatar: 'img/avatar_2.jpg',
-
+        ultimoAccesso:''
       },
       {
         nome: 'Fabrizio',
         avatar: 'img/avatar_3.jpg',
+        ultimoAccesso:''
       },
       {
         nome: 'Alessandra',
         avatar: 'img/avatar_6.jpg',
+        ultimoAccesso:''
       },
       {
         nome: 'Enrico',
         avatar: 'img/avatar_7.jpg',
+        ultimoAccesso:''
       },
       {
         nome: 'Giacomo',
         avatar: 'img/avatar_8.jpg',
+        ultimoAccesso:''
       }
-    ]
+    ],
+    listInput: [],
+    input: '',
+  },
+  methods:{
+    takeInput: function(){
+      this.listInput.push(this.input);
+      this.input = '';
+    }
   }
 })
